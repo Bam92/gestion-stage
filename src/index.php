323f6c68a94c4a25abb8c0a0e501f2241cc94814
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2021-03-19 20:47:08 +0200

    display attendancy on a table

--- src/index.php
+++ src/index.php
@@ -6,18 +6,23 @@
 
     if (isset($_POST['submit'])) {
 
-        $status = ($_POST['status'] == "present") ? 1 : 0;
-        $new_attendance = array(
-            "studentId" => $_POST['student'],
-            "date" => $_POST['date'],
-            "status" => $status
-        );
+        if (!empty($_POST['studentId'])) {
+            foreach ($_POST['studentId'] as $student_one) {
+                $status = ($_POST['status-' . $student_one] == "present") ? 1 : 0;
 
-        $sql = "INSERT INTO attendance (studentId, attendance_date, status) VALUES (:studentId, :date, :status)";
-        $req = $db->prepare($sql);
+                $new_attendance = array(
+                    "studentId" => $student_one,
+                    "date" => $_POST['attendance_date'],
+                    "status" => $status
+                );
 
-        $req->execute($new_attendance);
-        echo "Student added successfully!";
+                $sql = "INSERT INTO attendance (studentId, attendance_date, status) VALUES (:studentId, :date, :status)";
+                $req = $db->prepare($sql);
+
+                $req->execute($new_attendance);
+            }
+            echo "Student added successfully!";
+        }
     }
 
     function list_students()
@@ -29,29 +34,40 @@
     ?>
  <p><a href="./new_student.php">Ajouter nouveau stagiaire</a></p>
  <form method="post">
-     <input type="date" name="date"><br>
-     <select name="student" id="student">
-
-         <?php
-            $list = list_students();
-
-            foreach ($list as $student) { ?>
-
-         <option value=<?= $student['id'];   ?>>
-             <?= $student['name'];   ?>
-         </option>
-         <?php } ?>
-     </select>
-
      <div>
-         <input type="radio" id="present" name="status" value="present" checked>
-         <label for="present">Present</label>
+         <input type="date" name="attendance_date">
      </div>
-
      <div>
-         <input type="radio" id="absent" name="status" value="absent">
-         <label for="absent">Absent</label>
-     </div>
+         <table cellpadding="10" cellspacing="1">
+             <thead>
+                 <tr>
+                     <th>Etudiant</th>
+                     <th>Présent</th>
+                     <th>Absent</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <?php
+                    $list = list_students();
 
-     <input type="submit" name="submit" value="Enregistrer">
+                    foreach ($list as $student) { ?>
+                 <tr>
+                     <td>
+                         <input type="hidden" name="studentId[]" value=<?= $student['id']; ?>>
+                         <?= $student['first_name'], " ", $student['name'], " ", $student['last_name'];   ?>
+                     </td>
+                     <td>
+                         <input type="radio" id="present" name="status-<?= $student['id']; ?>" value="present" checked>
+                     </td>
+                     <td>
+                         <input type="radio" id="absent" name="status-<?= $student['id']; ?>" value="absent">
+                     </td>
+                 </tr>
+                 <?php } ?>
+             </tbody>
+         </table>
+     </div>
+     <div>
+         <input type="submit" name="submit" value="Enregistrer">
+     </div>
  </form>
