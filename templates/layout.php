67202b02b78799944ed7f717629ec0243ebcc0b8
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2021-05-14 19:23:11 +0200

    style: use font awesome CDN instead of local files

--- templates/layout.php
+++ templates/layout.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/all.css">
     <link rel="stylesheet" href="../public/style.css">
+    <script src="https://kit.fontawesome.com/f47cc0a207.js" crossorigin="anonymous"></script>
     <title><?= $title ?></title>
 </head>
 
@@ -18,6 +18,5 @@
     <main>
         <?= $content ?>
     </main>
-    <script src="../public/script.js"></script>
 
 </html>
